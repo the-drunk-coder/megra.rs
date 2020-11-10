ebb52ddc87fc9d62973c47ce2d3587dd2b7c55a3
nik <nik@parkellipsen.de>
2020-11-10 18:58:15 +0100

    a more sophisticated repl

--- src/main.rs
+++ src/main.rs
@@ -25,23 +25,25 @@ use std::io;
 use crate::scheduler::{Scheduler, SchedulerData};
 use crate::generator::Generator;
 use crate::session::Session;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() -> Result<(), anyhow::Error> {
-    
+
     let host = cpal::host_from_id(cpal::available_hosts()
-		       .into_iter()
-		       .find(|id| *id == cpal::HostId::Jack)
-		       .expect(
-			   "make sure --features jack is specified. only works on OSes where jack is available",
-		       )).expect("jack host unavailable");
-	    
+				  .into_iter()
+				  .find(|id| *id == cpal::HostId::Jack)
+				  .expect(
+				      "make sure --features jack is specified. only works on OSes where jack is available",
+				  )).expect("jack host unavailable");
+
     let device = host
         .default_output_device()
         .expect("failed to find a default output device");
 
     let config:cpal::SupportedStreamConfig = device.default_output_config().unwrap();
     let sample_format = config.sample_format();
-    
+
     match sample_format  {
         cpal::SampleFormat::F32 => run::<f32>(&device, &config.into())?,
         cpal::SampleFormat::I16 => run::<i16>(&device, &config.into())?,
@@ -57,7 +59,7 @@ where
 {
     let sample_rate = config.sample_rate.0 as f32;
     let channels = config.channels as usize;
-    
+
     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
 
     let ruffbox = Arc::new(Mutex::new(Ruffbox::<512>::new()));
@@ -78,29 +80,54 @@ where
         err_fn,
     )?;
     stream.play()?;
-    
+
     let mut session = Session::new();
 
-    loop {
-	let mut input = String::new();
-	io::stdin().read_line(&mut input)?;
-
-	let mut pfa_in = parser::eval_from_str(&input).unwrap();
-
-	match pfa_in {
-	    parser::Expr::Constant(parser::Atom::MarkovSequenceGenerator(p)) => {
-		
-		let mut gen = Box::new(Generator {
-		    name: "ho".to_string(),
-		    root_generator: p,
-		    processors: Vec::new()		
-		});
-
-		session.start_generator(gen, Arc::clone(&ruffbox))
-	    },
-	    _ => println!("unknown")		
-	}
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
     }
-    
-    //Ok(())                    
+
+    loop {
+        let readline = rl.readline("megra>> ");
+        match readline {
+            Ok(line) => {
+		let mut pfa_in = parser::eval_from_str(&line.as_str()).unwrap();
+
+		match pfa_in {
+		    parser::Expr::Constant(parser::Atom::MarkovSequenceGenerator(p)) => {
+			
+			let mut gen = Box::new(Generator {
+			    name: "ho".to_string(),
+			    root_generator: p,
+			    processors: Vec::new()
+			});
+		
+			session.start_generator(gen, Arc::clone(&ruffbox))
+		    },
+		    _ => println!("unknown")
+		}
+		
+		rl.add_history_entry(line.as_str());
+				
+		println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+
+    rl.save_history("history.txt").unwrap();
+    Ok(())
 }
