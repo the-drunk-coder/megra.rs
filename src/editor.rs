f0ff1578fcb9fa44ce46490c2832a450f9ca627c
nik <nik@parkellipsen.de>
2020-12-20 00:34:34 +0100

    egui based text editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,14 @@
 use std::sync::*;
 use parking_lot::Mutex;
-use crate::session::OutputMode;
 use ruffbox_synth::ruffbox::Ruffbox;
 
-use crate::builtin_types::*;
 mod megra_editor;
 use megra_editor::MegraEditor;
+
+use crate::session::{Session, OutputMode};
+use crate::builtin_types::*;
 use crate::parser;
+use crate::interpreter;
 
 pub fn run_editor<const BUFSIZE:usize, const NCHAN:usize>(ruffbox: &Arc<Mutex<Ruffbox<BUFSIZE, NCHAN>>>, mode: OutputMode) {
 
@@ -16,16 +18,20 @@ pub fn run_editor<const BUFSIZE:usize, const NCHAN:usize>(ruffbox: &Arc<Mutex<Ru
     // Restore editor from file, or create new editor:
     let mut app: MegraEditor = egui::app::get_value(&storage, egui::app::APP_KEY).unwrap_or_default();
 
-    let sample_set = SampleSet::new();
-    let parts_store = PartsStore::new();
+    let mut sample_set = SampleSet::new();
+    let mut parts_store = PartsStore::new();
+    let session = Arc::new(Mutex::new(Session::with_mode(mode)));
+    let ruffbox2 = Arc::clone(ruffbox);
         
     let callback_ref:Arc<Mutex<dyn FnMut(&String)>> = Arc::new(Mutex::new(
 	move |text: &String| {
 	    println!("{}", text);
 	    let pfa_in = parser::eval_from_str(text, &sample_set, &parts_store, mode);
 	    match pfa_in {
-		Ok(pfa) => {println!("valid!")},
-		Err(_) => {println!("error!")},
+		Ok(pfa) => {
+		    interpreter::interpret(&session, &mut sample_set, &mut parts_store, pfa, &ruffbox2);
+		},
+		Err(_) => {println!("could not parse this!")},
 	    }
 	}
     ));
