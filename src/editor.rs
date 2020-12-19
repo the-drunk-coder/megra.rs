621e9d2c8ff8a76cb00994a985d92441a795d35d
nik <nik@parkellipsen.de>
2020-12-20 00:24:58 +0100

    the callback works :D

--- src/editor.rs
+++ src/editor.rs
@@ -1,22 +1,36 @@
-use std::sync;
+use std::sync::*;
 use parking_lot::Mutex;
-
 use crate::session::OutputMode;
-
 use ruffbox_synth::ruffbox::Ruffbox;
 
+use crate::builtin_types::*;
 mod megra_editor;
 use megra_editor::MegraEditor;
+use crate::parser;
 
-
-pub fn run_editor<const BUFSIZE:usize, const NCHAN:usize>(ruffbox: &sync::Arc<Mutex<Ruffbox<BUFSIZE, NCHAN>>>, mode: OutputMode) {
-    let title = "Mégra Editor";
+pub fn run_editor<const BUFSIZE:usize, const NCHAN:usize>(ruffbox: &Arc<Mutex<Ruffbox<BUFSIZE, NCHAN>>>, mode: OutputMode) {
 
     // Persist app state to file:
     let storage = egui_glium::storage::FileStorage::from_path(".megra_edit.json");
     
     // Restore editor from file, or create new editor:
-    let app: MegraEditor = egui::app::get_value(&storage, egui::app::APP_KEY).unwrap_or_default();
+    let mut app: MegraEditor = egui::app::get_value(&storage, egui::app::APP_KEY).unwrap_or_default();
 
-    egui_glium::run(title, Box::new(storage), Box::new(app));
+    let sample_set = SampleSet::new();
+    let parts_store = PartsStore::new();
+        
+    let callback_ref:Arc<Mutex<dyn FnMut(&String)>> = Arc::new(Mutex::new(
+	move |text: &String| {
+	    println!("{}", text);
+	    let pfa_in = parser::eval_from_str(text, &sample_set, &parts_store, mode);
+	    match pfa_in {
+		Ok(pfa) => {println!("valid!")},
+		Err(_) => {println!("error!")},
+	    }
+	}
+    ));
+    
+    app.set_callback(&callback_ref);
+    
+    egui_glium::run(Box::new(storage), Box::new(app));
 }
--- src/editor/megra_editor.rs
+++ src/editor/megra_editor.rs
@@ -1,40 +1,59 @@
+use std::sync::*;
+use parking_lot::Mutex;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct MegraEditor {
     content: String,
+    #[serde(skip)]
+    callback: Option<Arc<Mutex<dyn FnMut(&String)>>>,
 }
+
 
 impl Default for MegraEditor {
     fn default() -> Self {
         Self {
             content: "(sx 'ga #t (infer 'troll :events 'a (saw 400) :rules (rule 'a 'a 100 400)))".to_owned(),
+	    callback: None,
         }
     }
 }
 
+impl MegraEditor {
+    pub fn set_callback(&mut self, callback: &Arc<Mutex<dyn FnMut(&String)>>) {		    
+	self.callback = Some(Arc::clone(callback));
+    }    
+}
+
 impl egui::app::App for MegraEditor {
+    fn name(&self) -> &str {
+        "Mégra Editor"
+    }
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn ui(
         &mut self,
-        ctx: &std::sync::Arc<egui::Context>,
+        ctx: &egui::CtxRef,
         integration_context: &mut egui::app::IntegrationContext,
     ) {
-        let MegraEditor { content } = self;
-
+       	
         // Example used in `README.md`.
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Mégra Editor");
 
-	    let tx = egui::TextEdit::multiline(content).desired_rows(31).desired_width(640.0);
-	    
-            ui.horizontal(|ui| {                
+	    let tx = if let Some(cb) = self.callback.as_ref() {
+		egui::CallbackTextEdit::multiline(&mut self.content).desired_rows(31).desired_width(640.0).eval_callback(&cb)		
+	    } else {
+		egui::CallbackTextEdit::multiline(&mut self.content).desired_rows(31).desired_width(640.0)
+	    };
+	    	    
+	    ui.horizontal(|ui| {                
 		ui.add(tx)
             });
-                        
+            
         });
-
-        integration_context.output.window_size = Some(egui::Vec2::new(640.0, 480.0)); // resize the window to be just the size we need it to be
+	
+	integration_context.output.window_size = Some(egui::Vec2::new(640.0, 480.0)); // resize the window to be just the size we need it to be
     }
 
     fn on_exit(&mut self, storage: &mut dyn egui::app::Storage) {
