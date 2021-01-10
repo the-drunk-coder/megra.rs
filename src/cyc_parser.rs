773850807098418fa41767a08dffe5d9c18f2989
nik <nik@parkellipsen.de>
2021-01-10 15:34:52 +0100

    cyc parser tests running so far

--- src/cyc_parser.rs
+++ src/cyc_parser.rs
@@ -4,7 +4,7 @@ use nom::{
     combinator::map,
     error::VerboseError,
     multi::{separated_list0, separated_list1},
-    sequence::separated_pair,
+    sequence::{preceded, separated_pair},
     IResult,
 };
 
@@ -18,18 +18,16 @@ use crate::sample_set::SampleSet;
 ///////////////////////////
 
 
-fn parse_cyc_builtin<'a>(i: &'a str) -> IResult<&'a str, BuiltIn, VerboseError<&'a str>> {        
-    parse_events(i)
+fn parse_cyc_event<'a>(i: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a str>> {        
+    map(map(parse_events, Atom::BuiltIn), |atom| Expr::Constant(atom))(i)
 }
-
 
 /// atoms within the cyc sublanguage
 fn parse_cyc_atom<'a>(i: &'a str) -> IResult<&'a str, Atom, VerboseError<&'a str>> {
     alt((	
 	parse_bool,
-	map(parse_cyc_builtin, Atom::BuiltIn),
 	parse_float, // parse after builtin, otherwise the beginning of "infer" would be parsed as "inf" (the float val)
-	parse_symbol,	
+	parse_symbol,
     ))(i)
 }
 
@@ -38,15 +36,21 @@ fn parse_cyc_constant<'a>(i: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a
 }
 
 fn parse_cyc_application<'a>(i: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a str>> {
-    map(separated_pair(alt((parse_cyc_constant, parse_custom)), tag(":"), separated_list0(tag(":"), parse_cyc_constant)),
-	|(head, tail)| {
-	    Expr::Application(Box::new(head), tail)
-	})(i)    
+    alt((map(separated_pair(alt((parse_cyc_event, parse_custom)), tag(":"), separated_list0(tag(":"), parse_cyc_constant)),
+	     |(head, tail)| {
+		 Expr::Application(Box::new(head), tail)
+	     }),
+	 map(alt((parse_cyc_event, parse_custom)), |head| { Expr::Application(Box::new(head), Vec::new())})
+	 ))(i)    
+}
+
+fn parse_cyc_param<'a>(i: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a str>> {
+    preceded(tag(":"), parse_cyc_constant)(i)
 }
 
 /// We tie them all together again, making a top-level expression parser!
 fn parse_cyc_expr<'a>(i: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a str>> {
-    alt((parse_cyc_application, parse_cyc_constant))(i)
+    alt((parse_cyc_param, parse_cyc_application))(i)
 }
 
 fn parse_cyc<'a>(i: &'a str) -> IResult<&'a str, Vec<Expr>, VerboseError<&'a str>> {
@@ -92,23 +96,89 @@ mod tests {
 	
 	match eval_cyc_from_str("saw:200 ~ ~ ~", &sample_set, &parts_store, OutputMode::Stereo) {
 	    Ok(o) => {
+		assert!(o.len() == 4);
+		
 		match &o[0] {
 		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "saw"),
 		    _ => assert!(false)					    
 		}
 
 		match &o[1] {
-		    Some(Expr::Constant(Atom::BuiltIn(BuiltIn::Silence))) => assert!(true),
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
 		    _ => assert!(false)
 		}
 
 		match &o[2] {
-		    Some(Expr::Constant(Atom::BuiltIn(BuiltIn::Silence))) => assert!(true),
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
 		    _ => assert!(false)
 		}
 		
 		match &o[3] {
-		    Some(Expr::Constant(Atom::BuiltIn(BuiltIn::Silence))) => assert!(true),
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
+		    _ => assert!(false)
+		}
+	    },
+	    Err(_) => assert!(false),
+	}
+    }
+
+    #[test]
+    fn test_basic_cyc_noparam() {
+	let sample_set = SampleSet::new();
+	let parts_store = PartsStore::new();
+	
+	match eval_cyc_from_str("saw ~ ~ ~", &sample_set, &parts_store, OutputMode::Stereo) {
+	    Ok(o) => {
+		assert!(o.len() == 4);
+		
+		match &o[0] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "saw"),
+		    _ => assert!(false)					    
+		}
+
+		match &o[1] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
+		    _ => assert!(false)
+		}
+
+		match &o[2] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
+		    _ => assert!(false)
+		}
+		
+		match &o[3] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
+		    _ => assert!(false)
+		}
+	    },
+	    Err(_) => assert!(false),
+	}
+    }
+
+    #[test]
+    fn test_param_only() {
+	let sample_set = SampleSet::new();
+	let parts_store = PartsStore::new();
+	
+	match eval_cyc_from_str(":200 ~ ~ ~", &sample_set, &parts_store, OutputMode::Stereo) {
+	    Ok(o) => {
+		match &o[0] {
+		    Some(Expr::Constant(Atom::Float(f))) => assert!(*f == 200.0),
+		    _ => assert!(false)					    
+		}
+
+		match &o[1] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
+		    _ => assert!(false)
+		}
+
+		match &o[2] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
+		    _ => assert!(false)
+		}
+		
+		match &o[3] {
+		    Some(Expr::Constant(Atom::SoundEvent(e))) => assert!(e.name == "silence"),
 		    _ => assert!(false)
 		}
 	    },
