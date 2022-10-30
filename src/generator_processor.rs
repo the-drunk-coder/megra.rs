8fe001bc40209148f0f8dff0b0eaad416a1bea5d
Niklas Reppel <nik@parkellipsen.de>
2022-10-30 18:01:03 +0100

    comments

--- src/generator_processor.rs
+++ src/generator_processor.rs
@@ -18,7 +18,11 @@ pub enum GeneratorProcessorState {
     None,
 }
 
+/// the generator processor only needs to implement
+/// a subset of the methods available ...
 pub trait GeneratorProcessor: GeneratorProcessorClone {
+    /// implement this if you want to modify the previous
+    /// processor's event stream
     fn process_events(
         &mut self,
         _events: &mut Vec<InterpretableEvent>,
@@ -26,6 +30,8 @@ pub trait GeneratorProcessor: GeneratorProcessorClone {
     ) {
         /* pass by default */
     }
+    /// implement this if you need to modify the previous
+    /// processor's structure 
     fn process_generator(
         &mut self,
         _generator: &mut Generator,
@@ -33,6 +39,8 @@ pub trait GeneratorProcessor: GeneratorProcessorClone {
     ) {
         /* pass by default */
     }
+    /// implement this if you need to modify the transitions
+    /// between events ...
     fn process_transition(
         &mut self,
         _transition: &mut StaticEvent,
@@ -41,18 +49,26 @@ pub trait GeneratorProcessor: GeneratorProcessorClone {
         /* pass by default */
     }
 
+    /// implement this if the processor has a state, such as a step
+    /// counter
     fn set_state(&mut self, _: GeneratorProcessorState) {
         /* processors are stateless by defalt */
     }
 
+    /// implement this if the processor has a state, such as a step
+    /// counter
     fn get_state(&self) -> GeneratorProcessorState {
         // processors are stateless by default
         GeneratorProcessorState::None
     }
 
+    /// if the processor holds something that can be visualized
+    /// such as a markov chain ...
     fn visualize_if_possible(&mut self, _vis_client: &sync::Arc<VisualizerClient>) {
         /* most won't need this */
     }
+
+    /// only if visualization is possible ...
     fn clear_visualization(&self, _vis_client: &sync::Arc<VisualizerClient>) {
         /* most won't need this */
     }
