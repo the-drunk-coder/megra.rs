be7524ac6f50db4fd2f4a275e32090961bd595cc
Niklas Reppel <nik@parkellipsen.de>
2023-05-05 23:53:31 +0200

    a more flexible loop parser

--- src/parser/eval/constructors/loop.rs
+++ src/parser/eval/constructors/loop.rs
@@ -1,47 +1,45 @@
 use crate::builtin_types::*;
+use crate::cyc_parser;
 use crate::event::*;
 use crate::generator::Generator;
 use crate::markov_sequence_generator::MarkovSequenceGenerator;
 use crate::parameter::*;
-
+use crate::sample_set::SampleAndWavematrixSet;
+use crate::session::OutputMode;
 use ruffbox_synth::building_blocks::SynthParameterLabel;
 use std::collections::{BTreeSet, HashMap};
 use std::sync;
 use vom_rs::pfa::{Pfa, Rule};
 
 use crate::parser::{BuiltIn, EvaluatedExpr, FunctionMap};
-use crate::{OutputMode, SampleAndWavematrixSet};
+
 use parking_lot::Mutex;
 
-/// construct a simple loop of events ... no tricks here
 pub fn a_loop(
-    _: &FunctionMap,
+    functions: &FunctionMap,
     tail: &mut Vec<EvaluatedExpr>,
     global_parameters: &sync::Arc<GlobalParameters>,
-    _: &sync::Arc<Mutex<SampleAndWavematrixSet>>,
-    _: OutputMode,
+    sample_set: &sync::Arc<Mutex<SampleAndWavematrixSet>>,
+    out_mode: OutputMode,
 ) -> Option<EvaluatedExpr> {
-    let mut tail_drain = tail.drain(..);
+    let mut tail_drain = tail.drain(..).peekable();
 
     // ignore function name in this case
     tail_drain.next();
 
     // name is the first symbol
-    let name = if let Some(EvaluatedExpr::Symbol(n)) = tail_drain.next() {
-        n
+    let name = if let Some(EvaluatedExpr::Symbol(n)) = tail_drain.peek() {
+        n.clone()
     } else {
         "".to_string()
     };
 
-    // collect final events and durations in their position in the list
-    let mut ev_vecs = Vec::new();
-    let mut dur_vec: Vec<DynVal> = Vec::new();
+    tail_drain.next();
 
-    let mut keep_root = false;
-
-    let dur: DynVal = if let ConfigParameter::Numeric(d) = global_parameters
+    // get the default global duration ...
+    let mut dur: DynVal = if let ConfigParameter::Numeric(d) = global_parameters
         .entry(BuiltinGlobalParameters::DefaultDuration)
-        .or_insert(ConfigParameter::Numeric(200.0))
+        .or_insert(ConfigParameter::Numeric(200.0)) // one "bar" in traditional terms ...
         .value()
     {
         DynVal::with_value(*d)
@@ -49,17 +47,124 @@ pub fn a_loop(
         unreachable!()
     };
 
+    // chances for modifiers ...
+    let mut repetition_chance: f32 = 0.0;
+    let mut randomize_chance: f32 = 0.0;
+    let mut max_repetitions: f32 = 0.0;
+
+    // collect event abbreviations ...
+    let mut collect_events = false;
+    // get a template ...
+    let mut collect_template = false;
+    let mut template_evs = Vec::new();
+
+    // collect mapped events, i.e. :events 'a (saw 200) ...
+    let mut collected_evs = Vec::new();
+    let mut collected_mapping = HashMap::<String, Vec<SourceEvent>>::new();
+    let mut cur_key: String = "".to_string();
+
+    // collect final events in their position in the cycle
+    let mut ev_vecs = Vec::new();
+    // transition durations ...
+    let mut dur_vec: Vec<DynVal> = Vec::new();
+    let mut keep_root = false;
+
     while let Some(c) = tail_drain.next() {
+        if collect_template {
+            match c {
+                EvaluatedExpr::Symbol(s) => {
+                    template_evs.push(s);
+                    continue;
+                }
+                _ => {
+                    collect_template = false;
+                }
+            }
+        }
+
+        if collect_events {
+            match c {
+                EvaluatedExpr::Symbol(ref s) => {
+                    if !cur_key.is_empty() && !collected_evs.is_empty() {
+                        //println!("found event {}", cur_key);
+                        collected_mapping.insert(cur_key.clone(), collected_evs.clone());
+                        collected_evs.clear();
+                    }
+                    cur_key = s.clone();
+                    continue;
+                }
+                EvaluatedExpr::BuiltIn(BuiltIn::SoundEvent(e)) => {
+                    collected_evs.push(SourceEvent::Sound(e));
+                    continue;
+                }
+                EvaluatedExpr::BuiltIn(BuiltIn::ControlEvent(e)) => {
+                    collected_evs.push(SourceEvent::Control(e));
+                    continue;
+                }
+                _ => {
+                    if !cur_key.is_empty() && !collected_evs.is_empty() {
+                        //println!("found event {}", cur_key);
+                        collected_mapping.insert(cur_key.clone(), collected_evs.clone());
+                    }
+                    collect_events = false;
+                }
+            }
+        }
+
         match c {
+            EvaluatedExpr::Keyword(k) => match k.as_str() {
+                "dur" => match tail_drain.next() {
+                    Some(EvaluatedExpr::Float(n)) => {
+                        dur = DynVal::with_value(n);
+                    }
+                    Some(EvaluatedExpr::BuiltIn(BuiltIn::Parameter(p))) => {
+                        dur = p;
+                    }
+                    _ => {}
+                },
+                "rep" => {
+                    if let Some(EvaluatedExpr::Float(n)) = tail_drain.peek() {
+                        repetition_chance = *n;
+                        tail_drain.next();
+                    }
+                }
+                "rnd" => {
+                    if let Some(EvaluatedExpr::Float(n)) = tail_drain.peek() {
+                        randomize_chance = *n;
+                        tail_drain.next();
+                    }
+                }
+                "max-rep" => {
+                    if let Some(EvaluatedExpr::Float(n)) = tail_drain.peek() {
+                        max_repetitions = *n;
+                        tail_drain.next();
+                    }
+                }
+                "events" => {
+                    collect_events = true;
+                    continue;
+                }
+                "map" => {
+                    collect_template = true;
+                    continue;
+                }
+                "keep" => {
+                    if let Some(EvaluatedExpr::Boolean(b)) = tail_drain.peek() {
+                        keep_root = *b;
+                        tail_drain.next();
+                    }
+                }
+                _ => println!("{k}"),
+            },
             EvaluatedExpr::BuiltIn(BuiltIn::SoundEvent(e)) => {
                 ev_vecs.push(vec![SourceEvent::Sound(e)]);
+                // one duration for every event
                 dur_vec.push(dur.clone());
-                continue;
             }
             EvaluatedExpr::BuiltIn(BuiltIn::ControlEvent(e)) => {
                 ev_vecs.push(vec![SourceEvent::Control(e)]);
+                // one duration for every event
                 dur_vec.push(dur.clone());
-                continue;
             }
             EvaluatedExpr::Float(f) => {
                 if !dur_vec.is_empty() {
@@ -68,10 +173,44 @@ pub fn a_loop(
                     dur_vec.push(DynVal::with_value(f));
                 }
             }
-            EvaluatedExpr::Keyword(k) => {
-                if k == "keep" {
-                    if let Some(EvaluatedExpr::Boolean(b)) = tail_drain.next() {
-                        keep_root = b;
+            EvaluatedExpr::String(d) => {
+                let mut parsed_cycle = cyc_parser::eval_cyc_from_str(
+                    &d,
+                    functions,
+                    sample_set,
+                    out_mode,
+                    &template_evs,
+                    &collected_mapping,
+                    global_parameters,
+                );
+                if parsed_cycle.is_empty() {
+                    println!("couldn't parse cycle");
+                }
+                for mut cyc_evs in parsed_cycle.drain(..) {
+                    match cyc_evs.as_slice() {
+                        [cyc_parser::CycleResult::Duration(dur)] => {
+                            // the loop, on the other hand, has variable
+                            // durations between events ...
+                            *dur_vec.last_mut().unwrap() = DynVal::with_value(*dur);
+                        }
+                        _ => {
+                            let mut pos_vec = Vec::new();
+                            dur_vec.push(dur.clone());
+
+                            for ev in cyc_evs.drain(..) {
+                                match ev {
+                                    cyc_parser::CycleResult::SoundEvent(s) => {
+                                        pos_vec.push(SourceEvent::Sound(s))
+                                    }
+                                    cyc_parser::CycleResult::ControlEvent(c) => {
+                                        pos_vec.push(SourceEvent::Control(c))
+                                    }
+                                    _ => {}
+                                }
+                            }
+
+                            ev_vecs.push(pos_vec);
+                        }
                     }
                 }
             }
@@ -82,61 +221,117 @@ pub fn a_loop(
     let mut event_mapping = HashMap::<char, Vec<SourceEvent>>::new();
     let mut duration_mapping = HashMap::<(char, char), Event>::new();
 
+    // re-generate pfa if necessary, now that we have collected all the info ...
     let pfa = if !keep_root {
         // generated ids
         let mut last_char: char = '1';
-        let first_char: char = last_char;
+        let first_char = last_char;
 
         // collect cycle rules
         let mut rules = Vec::new();
-        let len = ev_vecs.len() - 1;
 
-        for (count, ev) in ev_vecs.drain(..).enumerate() {
+        let mut count = 0;
+        let num_events = ev_vecs.len();
+        for ev in ev_vecs.drain(..) {
+            let next_char: char = std::char::from_u32(last_char as u32 + 1).unwrap();
+
             event_mapping.insert(last_char, ev);
 
-            if count < len {
-                let next_char: char = std::char::from_u32(last_char as u32 + 1).unwrap();
+            if count < num_events {
+                if repetition_chance > 0.0 {
+                    //println!("add rep chance");
+                    // repetition rule
+                    rules.push(Rule {
+                        source: vec![last_char],
+                        symbol: last_char,
+                        probability: repetition_chance / 100.0,
+                    });
 
-                let mut dur_ev = Event::with_name("transition".to_string());
-                dur_ev.params.insert(
-                    SynthParameterLabel::Duration,
-                    ParameterValue::Scalar(dur_vec[count].clone()),
-                );
+                    // next rule
+                    if count == num_events - 1 {
+                        rules.push(Rule {
+                            source: vec![last_char],
+                            symbol: first_char,
+                            probability: 1.0 - (repetition_chance / 100.0),
+                        });
+                    } else {
+                        rules.push(Rule {
+                            source: vec![last_char],
+                            symbol: next_char,
+                            probability: 1.0 - (repetition_chance / 100.0),
+                        });
+                    }
 
-                rules.push(Rule {
-                    source: vec![last_char],
-                    symbol: next_char,
-                    probability: 1.0,
-                });
+                    // endless repetition allowed per default ...
+                    if max_repetitions >= 2.0 {
+                        let mut max_rep_source = Vec::new();
+                        for _ in 0..max_repetitions as usize {
+                            max_rep_source.push(last_char);
+                        }
+                        // max repetition rule
+                        if count == num_events - 1 {
+                            rules.push(Rule {
+                                source: max_rep_source,
+                                symbol: first_char,
+                                probability: 1.0,
+                            });
+                        } else {
+                            rules.push(Rule {
+                                source: max_rep_source,
+                                symbol: next_char,
+                                probability: 1.0,
+                            });
+                        }
+                    }
+                } else if count == num_events - 1 {
+                    rules.push(Rule {
+                        source: vec![last_char],
+                        symbol: first_char,
+                        probability: 1.0,
+                    });
+                } else {
+                    rules.push(Rule {
+                        source: vec![last_char],
+                        symbol: next_char,
+                        probability: 1.0,
+                    });
+                }
 
-                duration_mapping.insert((last_char, next_char), dur_ev);
-
-                last_char = next_char;
+                if count < num_events - 1 {
+                    let mut dur_ev = Event::with_name("transition".to_string());
+                    dur_ev.params.insert(
+                        SynthParameterLabel::Duration,
+                        ParameterValue::Scalar(dur_vec[count].clone()),
+                    );
+                    duration_mapping.insert((last_char, next_char), dur_ev);
+                    last_char = next_char;
+                }
             }
+
+            count += 1;
         }
 
-        let mut dur_ev = Event::with_name("transition".to_string());
-        dur_ev.params.insert(
-            SynthParameterLabel::Duration,
-            ParameterValue::Scalar(if let Some(ldur) = dur_vec.last() {
-                ldur.clone()
-            } else {
-                dur.clone()
-            }),
-        );
+        // if our cycle isn't empty ...
+        if count != 0 {
+            // create duration event (otherwise not needed ...)
+            let mut dur_ev = Event::with_name("transition".to_string());
+            dur_ev.params.insert(
+                SynthParameterLabel::Duration,
+                ParameterValue::Scalar(dur_vec.last().unwrap().clone()),
+            );
+            duration_mapping.insert((last_char, first_char), dur_ev);
+        }
 
-        // close the loop
-        rules.push(Rule {
-            source: vec![last_char],
-            symbol: first_char,
-            probability: 1.0,
-        });
+        let mut tmp = Pfa::<char>::infer_from_rules(&mut rules, true);
 
-        duration_mapping.insert((last_char, first_char), dur_ev);
-
-        // don't remove orphans here because the first state is technically
-        // "orphan"
-        Pfa::<char>::infer_from_rules(&mut rules, true)
+        // this seems to be heavy ...
+        // what's so heavy here ??
+        if randomize_chance > 0.0 {
+            //println!("add rnd chance");
+            tmp.randomize_edges(randomize_chance, randomize_chance);
+            tmp.rebalance();
+        }
+        tmp
     } else {
         Pfa::<char>::new()
     };
