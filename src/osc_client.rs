1989f3024717fa541ca3616544611c9076a5d8dc
Niklas Reppel <nik@parkellipsen.de>
2023-09-24 18:16:50 +0200

    extra comment

--- src/osc_client.rs
+++ src/osc_client.rs
@@ -6,6 +6,10 @@ use crate::{osc_sender::OscSender, visualizer_client::VisualizerClient};
 
 #[derive(Clone)]
 pub struct OscClient {
+    // probably this extra flag isn't all that necessary, but maybe
+    // it saves a bit of time if we just check the flag instead of
+    // trying to acquire the read lock on the client all the time?
+    // Or at least that's what I'm thinking ...
     pub vis_connected: sync::Arc<AtomicBool>,
     pub vis: sync::Arc<RwLock<Option<VisualizerClient>>>,
     pub custom: sync::Arc<DashMap<String, OscSender>>,
