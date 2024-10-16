3dce27f778d52f052af3d6189dfc47eeeedc8bd1
nik <nik@parkellipsen.de>
2024-10-16 19:22:14 +0200

    a different match approach

--- src/parser/eval/megra_match.rs
+++ src/parser/eval/megra_match.rs
@@ -17,15 +17,101 @@ pub fn megra_match(
     // ignore function name
     tail_drain.next();
 
-    if let Some(to_be_matched) = tail_drain.next() {
-        let mut matchees = Vec::new();
+    if let Some(temp) = tail_drain.next() {
+        let mut exprs = Vec::new();
 
         while let Some(n) = tail_drain.next() {
             if let Some(x) = tail_drain.next() {
-                matchees.push((n, x));
+                exprs.push((n, x));
             }
         }
-        Ok(EvaluatedExpr::Match(Box::new(to_be_matched), matchees))
+
+        if let EvaluatedExpr::Typed(t1) = temp {
+            for (comp, expr) in exprs {
+                match comp {
+                    EvaluatedExpr::Typed(TypedEntity::Comparable(t2)) => {
+                        if let TypedEntity::Comparable(ref ct1) = t1 {
+                            if *ct1 == t2 {
+                                return Ok(expr);
+                            }
+                        }
+                    }
+                    EvaluatedExpr::Comparator(t2) => match t2 {
+                        Comparator::GreaterEqual(x) => {
+                            if let TypedEntity::Comparable(ref ct1) = t1 {
+                                if *ct1 >= x {
+                                    return Ok(expr);
+                                }
+                            }
+                        }
+                        Comparator::Greater(x) => {
+                            if let TypedEntity::Comparable(ref ct1) = t1 {
+                                if *ct1 > x {
+                                    return Ok(expr);
+                                }
+                            }
+                        }
+                        Comparator::Equal(x) => {
+                            if let TypedEntity::Comparable(ref ct1) = t1 {
+                                if *ct1 == x {
+                                    return Ok(expr);
+                                }
+                            }
+                        }
+                        Comparator::LesserEqual(x) => {
+                            if let TypedEntity::Comparable(ref ct1) = t1 {
+                                if *ct1 <= x {
+                                    return Ok(expr);
+                                }
+                            }
+                        }
+                        Comparator::Lesser(x) => {
+                            if let TypedEntity::Comparable(ref ct1) = t1 {
+                                if *ct1 < x {
+                                    return Ok(expr);
+                                }
+                            }
+                        }
+                        Comparator::IsNumerical => {
+                            if matches!(t1, TypedEntity::Comparable(Comparable::Double(_)))
+                                || matches!(t1, TypedEntity::Comparable(Comparable::Float(_)))
+                                || matches!(t1, TypedEntity::Comparable(Comparable::Int32(_)))
+                                || matches!(t1, TypedEntity::Comparable(Comparable::Int64(_)))
+                            {
+                                return Ok(expr);
+                            }
+                        }
+
+                        Comparator::IsString => {
+                            if matches!(t1, TypedEntity::Comparable(Comparable::String(_))) {
+                                return Ok(expr);
+                            }
+                        }
+                        Comparator::IsSymbol => {
+                            if matches!(t1, TypedEntity::Comparable(Comparable::Symbol(_))) {
+                                return Ok(expr);
+                            }
+                        }
+                        Comparator::IsMap => {
+                            if matches!(t1, TypedEntity::Map(_)) {
+                                return Ok(expr);
+                            }
+                        }
+                        Comparator::IsVec => {
+                            if matches!(t1, TypedEntity::Vec(_)) {
+                                return Ok(expr);
+                            }
+                        }
+                    },
+                    _ => {}
+                }
+            }
+        };
+
+        // nothing matches
+        Ok(EvaluatedExpr::Typed(TypedEntity::Comparable(
+            Comparable::Boolean(false),
+        )))
     } else {
         Err(anyhow!("match - body empty"))
     }
